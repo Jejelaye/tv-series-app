f50f3aba934804755b22788a3b65052b671658f5
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2019-03-04 21:42:02 +0100

    Updates

---
+++ precache-manifest.a0851035661c2ad6b12be0480811cb22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "029be13773430f7ca6c2",
+    "url": "jejelaye/github.io/tv-series-app/static/css/main.d6876f8f.chunk.css"
+  },
+  {
+    "revision": "029be13773430f7ca6c2",
+    "url": "jejelaye/github.io/tv-series-app/static/js/main.029be137.chunk.js"
+  },
+  {
+    "revision": "94e4b4972fb2fb878c6c",
+    "url": "jejelaye/github.io/tv-series-app/static/js/1.94e4b497.chunk.js"
+  },
+  {
+    "revision": "9666fcb89d6b26a5b3be",
+    "url": "jejelaye/github.io/tv-series-app/static/js/runtime~main.9666fcb8.js"
+  },
+  {
+    "revision": "24b51230a1ec2f6d94db0d1cb41eb415",
+    "url": "jejelaye/github.io/tv-series-app/static/media/ajax-loader.24b51230.gif"
+  },
+  {
+    "revision": "7565e422cfba01924bea169176475a34",
+    "url": "jejelaye/github.io/tv-series-app/index.html"
+  }
+];
